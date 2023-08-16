d3cebb1d7b7c46285dfeb2ca9359976e1eaca999
faridullahak <faridullah.akbari@tekschool.us>
2023-08-16 11:54:18 -0400

    i hope i learn some

--- src/demo/TestClass.java
+++ src/demo/TestClass.java
@@ -1,5 +1,9 @@
 package demo;
 
 public class TestClass {
-
+public static void main(String[] args) {
+	System.out.println("Hello Guardians");
+	System.out.println("this is my second message for first project");
+	
+}
 }
